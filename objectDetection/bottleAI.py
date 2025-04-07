906b15f4bbbe5f860049d4ef30d1f21a768343bc
Owen G <50187784+potato-potato-potato-potato@users.noreply.github.com>
2025-04-06 17:58:45 -0700

    final

--- objectDetection/bottleAI.py
+++ objectDetection/bottleAI.py
@@ -21,11 +21,11 @@ def analyze_images_with_gemini(image_list, api_key, isadd):
 
     if isadd:
          contents = [
-                {"text": "what is this bottle/can/drink? only give a short response to name and flavor the bottle/can/drink. The response will be added to a database as a value. An e"}
+                 {"text": "what is this bottle/can/drink? only give a short response to name the bottle/can/drink. The response will be added to a database as a value. If you are unsure or can not make it out give the response: bottle"}
             ]
     else:
         contents = [
-                {"text": f"what is this bottle/can/drink? only give a short response to name and flavor the bottle/can/drink. pick from the following that best matches the image: {previous_drinks}"}
+                {"text": f"what is this bottle/can/drink? only give a short response to name of the bottle/can/drink. pick from the following that best matches the image: {previous_drinks}"}
             ]
 
 
